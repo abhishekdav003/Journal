a990635dc50ca23ccdc0df55a2663e99172d0581
Anup Chauhan <84135396+anupchauhanak6@users.noreply.github.com>
2026-01-27 21:07:59 +0530

    Refactor next.config.mjs to improve rewrite structure and formatting

--- client/main-app/next.config.mjs
+++ client/main-app/next.config.mjs
@@ -1,24 +1,47 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      "ui-avatars.com",
+      "res.cloudinary.com",
+      "www.transparenttextures.com",
+    ],
+  },
 
   async rewrites() {
     return [
       {
-        source: '/user/profile',
-        destination: '/student/profile',
+        source: "/user/profile",
+        destination: "/student/profile",
       },
       {
-        source: '/user/courses',
-        destination: '/student/courses',
+        source: "/user/courses",
+        destination: "/student/courses",
       },
       {
-        source: '/user/payments',
-        destination: '/student/payments',
+        source: "/user/payments",
+        destination: "/student/payments",
       },
       {
-        source: '/user/settings',
-        destination: '/student/settings',
+        source: "/user/settings",
+        destination: "/student/settings",
+      },
+      {
+        source: "/user/public-profile",
+        destination: "/student/public-profile",
+      },
+      {
+        source: "/user/photo",
+        destination: "/student/photo",
+      },
+      {
+        source: "/user/account-security",
+        destination: "/student/account-security",
+      },
+      {
+        source: "/user/close-account",
+        destination: "/student/close-account",
       },
     ];
   },
