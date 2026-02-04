a3677800a4b265089702b142aa32c4741e7ff5c3
Anup Chauhan <84135396+anupchauhanak6@users.noreply.github.com>
2026-02-04 14:19:21 +0530

    feat: enhance layout by adding decorative background elements for improved visual appeal

--- client/main-app/src/pages/_app.js
+++ client/main-app/src/pages/_app.js
@@ -6,7 +6,9 @@ import { AuthProvider } from "../context/AuthContext";
 export default function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
-      <div className="min-h-screen flex flex-col">
+      <div className="min-h-screen flex flex-col relative overflow-x-hidden">
+        <div className="h-127 w-128.5 bg-[#B00FF0] -top-64.25 -left-65  rounded-full blur-[200px] fixed"></div>
+        <div className="h-127 w-128.5 bg-[#B00FF0] bottom-15 -right-65  rounded-full blur-[200px] fixed"></div>
         <Header />
         <main className="flex-1">
           <Component {...pageProps} />
